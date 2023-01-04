c451fc47073a79e0f6b1f8d0cf230f2f50e37966
Asier Arranz <asierarranz@gmail.com>
2023-01-04 16:33:19 +0100

    Instructions removed to make it faster to execute

--- 3_Demos_in_loop/Demo_Playlist_editable.sh
+++ 3_Demos_in_loop/Demo_Playlist_editable.sh
@@ -1,5 +1,3 @@
-read -p "You will need to download the Pednet model first to try this demo, if you don't have it, execute Step1b and select the PedNet model to download. Also, please wait, the first execution will take longer.
-Press any key to continue..." 
 cd ~/jetson-inference/python/examples
 while :
 do
