38a1960c4f95486f51792a609e704ae4f3630774
Asier Arranz <asierarranz@gmail.com>
2023-01-04 16:28:05 +0100

    Removed unnecessary file

--- 2_Tests/Step2_Pedestrians_DetectNet (copy).sh
+++
@@ -1,4 +0,0 @@
-read -p "Please wait, the first execution will take longer.
-Press any key to continue..."
-cd ~/jetson-inference/python/examples
-python3 detectnet.py /opt/nvidia/vpi2/samples/assets/pedestrians.mp4
