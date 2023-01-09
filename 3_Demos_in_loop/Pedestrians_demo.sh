aa3a6a9d2f43d3bd9aad04cc571bb3bb6312fad5
Asier Arranz <asierarranz@gmail.com>
2023-01-09 12:29:28 +0100

    PedNet demo in loop

--- 3_Demos_in_loop/Pedestrians_demo.sh
+++ 3_Demos_in_loop/Pedestrians_demo.sh
@@ -1,4 +1,5 @@
-read -p "You will need to download the Pednet model first to try this demo, if you don't have it, execute Step1b and select the PedNet model to download. Also, please wait, the first execution will take longer.
+read -p "This demo will run the PedNet demo in loop.
+You will need to download the Pednet model first to try this demo, if you don't have it, execute Step1b and select the PedNet model to download. Also, please wait, the first execution will take longer.
 Press any key to continue..." 
 cd ~/jetson-inference/python/examples
 python3 detectnet_demo.py --network=pednet /opt/nvidia/vpi2/samples/assets/pedestrians.mp4
